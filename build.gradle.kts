d2fbbd6823d62df0f7068250e92cb59b8d4d694c
Konicai <71294714+Konicai@users.noreply.github.com>
2023-02-26 17:11:21 -0500

    Disable publication signing

--- build.gradle.kts
+++ build.gradle.kts
@@ -73,11 +73,13 @@ configure<PublishingExtension> {
             }
         }
 
+        /*
         signing {
             val signingKey = System.getenv("SIGNING_KEY")
             val signingPassword = System.getenv("SIGNING_PASSWORD")
             useInMemoryPgpKeys(signingKey, signingPassword)
             sign(publishing.publications["mavenJava"])
         }
+         */
     }
 }
